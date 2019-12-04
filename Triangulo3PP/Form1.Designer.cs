a2b3cdd4d463584e628259f0bc5da11360fe087e
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-12-04 12:07:11 +0000

    fimm

--- Triangulo3PP/Form1.Designer.cs
+++ Triangulo3PP/Form1.Designer.cs
@@ -53,10 +53,10 @@
             // perimetro
             // 
             this.perimetro.AutoSize = true;
-            this.perimetro.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.perimetro.Location = new System.Drawing.Point(480, 366);
+            this.perimetro.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perimetro.Location = new System.Drawing.Point(136, 165);
             this.perimetro.Name = "perimetro";
-            this.perimetro.Size = new System.Drawing.Size(194, 61);
+            this.perimetro.Size = new System.Drawing.Size(77, 20);
             this.perimetro.TabIndex = 39;
             this.perimetro.TabStop = true;
             this.perimetro.Text = "Perímetro";
@@ -65,10 +65,10 @@
             // area
             // 
             this.area.AutoSize = true;
-            this.area.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.area.Location = new System.Drawing.Point(269, 366);
+            this.area.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.area.Location = new System.Drawing.Point(79, 165);
             this.area.Name = "area";
-            this.area.Size = new System.Drawing.Size(110, 61);
+            this.area.Size = new System.Drawing.Size(51, 20);
             this.area.TabIndex = 38;
             this.area.TabStop = true;
             this.area.Text = "Área";
@@ -77,176 +77,177 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(585, 83);
+            this.label8.Location = new System.Drawing.Point(181, 38);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(62, 79);
+            this.label8.Size = new System.Drawing.Size(14, 16);
             this.label8.TabIndex = 37;
             this.label8.Text = "z";
+            this.label8.Click += new System.EventHandler(this.Label8_Click);
             // 
             // z3
             // 
-            this.z3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.z3.Location = new System.Drawing.Point(547, 299);
+            this.z3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z3.Location = new System.Drawing.Point(171, 124);
             this.z3.Name = "z3";
-            this.z3.Size = new System.Drawing.Size(127, 61);
+            this.z3.Size = new System.Drawing.Size(40, 24);
             this.z3.TabIndex = 36;
             // 
             // z2
             // 
-            this.z2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.z2.Location = new System.Drawing.Point(547, 232);
+            this.z2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z2.Location = new System.Drawing.Point(171, 91);
             this.z2.Name = "z2";
-            this.z2.Size = new System.Drawing.Size(127, 61);
+            this.z2.Size = new System.Drawing.Size(40, 24);
             this.z2.TabIndex = 35;
             // 
             // z1
             // 
-            this.z1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.z1.Location = new System.Drawing.Point(547, 165);
+            this.z1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z1.Location = new System.Drawing.Point(171, 57);
             this.z1.Name = "z1";
-            this.z1.Size = new System.Drawing.Size(127, 61);
+            this.z1.Size = new System.Drawing.Size(40, 24);
             this.z1.TabIndex = 34;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(105, 299);
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 124);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(158, 57);
+            this.label7.Size = new System.Drawing.Size(49, 16);
             this.label7.TabIndex = 33;
             this.label7.Text = "Ponto 3";
             // 
             // y3
             // 
-            this.y3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.y3.Location = new System.Drawing.Point(414, 299);
+            this.y3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y3.Location = new System.Drawing.Point(125, 124);
             this.y3.Name = "y3";
-            this.y3.Size = new System.Drawing.Size(127, 61);
+            this.y3.Size = new System.Drawing.Size(40, 24);
             this.y3.TabIndex = 32;
             // 
             // x3
             // 
-            this.x3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.x3.Location = new System.Drawing.Point(269, 299);
+            this.x3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x3.Location = new System.Drawing.Point(79, 124);
             this.x3.Name = "x3";
-            this.x3.Size = new System.Drawing.Size(139, 61);
+            this.x3.Size = new System.Drawing.Size(40, 24);
             this.x3.TabIndex = 31;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Red;
-            this.label6.Location = new System.Drawing.Point(447, 83);
+            this.label6.Location = new System.Drawing.Point(133, 38);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 79);
+            this.label6.Size = new System.Drawing.Size(14, 16);
             this.label6.TabIndex = 30;
             this.label6.Text = "y";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(308, 82);
+            this.label5.Location = new System.Drawing.Point(90, 38);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 79);
+            this.label5.Size = new System.Drawing.Size(15, 16);
             this.label5.TabIndex = 29;
             this.label5.Text = "x";
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(269, 518);
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(69, 232);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(216, 58);
+            this.button1.Size = new System.Drawing.Size(159, 41);
             this.button1.TabIndex = 28;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Maroon;
-            this.label4.Location = new System.Drawing.Point(289, 4);
+            this.label4.Location = new System.Drawing.Point(90, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(252, 79);
+            this.label4.Size = new System.Drawing.Size(57, 16);
             this.label4.TabIndex = 27;
             this.label4.Text = "Triângulo";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(105, 232);
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 57);
+            this.label3.Size = new System.Drawing.Size(49, 16);
             this.label3.TabIndex = 26;
             this.label3.Text = "Ponto 2";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(105, 165);
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 57);
+            this.label2.Size = new System.Drawing.Size(49, 16);
             this.label2.TabIndex = 25;
             this.label2.Text = "Ponto 1";
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 435);
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 193);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(692, 65);
+            this.label1.Size = new System.Drawing.Size(166, 28);
             this.label1.TabIndex = 24;
-            this.label1.Text = "label1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // y2
             // 
-            this.y2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.y2.Location = new System.Drawing.Point(414, 232);
+            this.y2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y2.Location = new System.Drawing.Point(125, 91);
             this.y2.Name = "y2";
-            this.y2.Size = new System.Drawing.Size(127, 61);
+            this.y2.Size = new System.Drawing.Size(40, 24);
             this.y2.TabIndex = 23;
             // 
             // x2
             // 
-            this.x2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.x2.Location = new System.Drawing.Point(269, 232);
+            this.x2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x2.Location = new System.Drawing.Point(79, 91);
             this.x2.Name = "x2";
-            this.x2.Size = new System.Drawing.Size(139, 61);
+            this.x2.Size = new System.Drawing.Size(40, 24);
             this.x2.TabIndex = 22;
             // 
             // y1
             // 
-            this.y1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.y1.Location = new System.Drawing.Point(414, 165);
+            this.y1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y1.Location = new System.Drawing.Point(125, 57);
             this.y1.Name = "y1";
-            this.y1.Size = new System.Drawing.Size(127, 61);
+            this.y1.Size = new System.Drawing.Size(40, 24);
             this.y1.TabIndex = 21;
             // 
             // x1
             // 
-            this.x1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.x1.Location = new System.Drawing.Point(269, 165);
+            this.x1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x1.Location = new System.Drawing.Point(79, 57);
             this.x1.Name = "x1";
-            this.x1.Size = new System.Drawing.Size(139, 61);
+            this.x1.Size = new System.Drawing.Size(40, 24);
             this.x1.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.ClientSize = new System.Drawing.Size(269, 279);
             this.Controls.Add(this.perimetro);
             this.Controls.Add(this.area);
             this.Controls.Add(this.label8);
--- Triangulo3PP/Form1.cs
+++ Triangulo3PP/Form1.cs
@@ -16,5 +16,27 @@ namespace Triangulo3PP
         {
             InitializeComponent();
         }
+
+        Triangulo T = new Triangulo();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.Y = Convert.ToDouble(y1.Text);
+            T.P1.Z = Convert.ToDouble(z1.Text);
+            T.P2.X = Convert.ToDouble(x2.Text);
+            T.P2.Y = Convert.ToDouble(y2.Text);
+            T.P2.Z = Convert.ToDouble(z2.Text);
+            T.P3.X = Convert.ToDouble(x3.Text);
+            T.P3.Y = Convert.ToDouble(y3.Text);
+            T.P3.Z = Convert.ToDouble(z3.Text);
+            if (area.Checked) label1.Text = T.Area.ToString();
+            else if (perimetro.Checked) label1.Text = T.Perimetro.ToString();
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
