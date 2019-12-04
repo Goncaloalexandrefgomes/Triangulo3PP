e0de62db5b636c6c99181fb3a93ecff2ef16615b
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-12-04 11:57:32 +0000

    Adicionar arquivos de projeto.

---
+++ Triangulo3PP/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Triangulo3PP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.perimetro = new System.Windows.Forms.RadioButton();
+            this.area = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.z3 = new System.Windows.Forms.TextBox();
+            this.z2 = new System.Windows.Forms.TextBox();
+            this.z1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // perimetro
+            // 
+            this.perimetro.AutoSize = true;
+            this.perimetro.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perimetro.Location = new System.Drawing.Point(480, 366);
+            this.perimetro.Name = "perimetro";
+            this.perimetro.Size = new System.Drawing.Size(194, 61);
+            this.perimetro.TabIndex = 39;
+            this.perimetro.TabStop = true;
+            this.perimetro.Text = "Perímetro";
+            this.perimetro.UseVisualStyleBackColor = true;
+            // 
+            // area
+            // 
+            this.area.AutoSize = true;
+            this.area.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.area.Location = new System.Drawing.Point(269, 366);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(110, 61);
+            this.area.TabIndex = 38;
+            this.area.TabStop = true;
+            this.area.Text = "Área";
+            this.area.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(585, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 79);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "z";
+            // 
+            // z3
+            // 
+            this.z3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z3.Location = new System.Drawing.Point(547, 299);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(127, 61);
+            this.z3.TabIndex = 36;
+            // 
+            // z2
+            // 
+            this.z2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z2.Location = new System.Drawing.Point(547, 232);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(127, 61);
+            this.z2.TabIndex = 35;
+            // 
+            // z1
+            // 
+            this.z1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z1.Location = new System.Drawing.Point(547, 165);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(127, 61);
+            this.z1.TabIndex = 34;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(105, 299);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 57);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "Ponto 3";
+            // 
+            // y3
+            // 
+            this.y3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y3.Location = new System.Drawing.Point(414, 299);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(127, 61);
+            this.y3.TabIndex = 32;
+            // 
+            // x3
+            // 
+            this.x3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x3.Location = new System.Drawing.Point(269, 299);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(139, 61);
+            this.x3.TabIndex = 31;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(447, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 79);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(308, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 79);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "x";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(269, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 58);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(289, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 79);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Triângulo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(105, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 57);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Ponto 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 57);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Ponto 1";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(692, 65);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // y2
+            // 
+            this.y2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y2.Location = new System.Drawing.Point(414, 232);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(127, 61);
+            this.y2.TabIndex = 23;
+            // 
+            // x2
+            // 
+            this.x2.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x2.Location = new System.Drawing.Point(269, 232);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(139, 61);
+            this.x2.TabIndex = 22;
+            // 
+            // y1
+            // 
+            this.y1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y1.Location = new System.Drawing.Point(414, 165);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(127, 61);
+            this.y1.TabIndex = 21;
+            // 
+            // x1
+            // 
+            this.x1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x1.Location = new System.Drawing.Point(269, 165);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(139, 61);
+            this.x1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.perimetro);
+            this.Controls.Add(this.area);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton perimetro;
+        private System.Windows.Forms.RadioButton area;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox z3;
+        private System.Windows.Forms.TextBox z2;
+        private System.Windows.Forms.TextBox z1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x1;
+    }
+}
+
